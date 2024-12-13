33317dace25dc61b27e32bc3eebc35cc48fb05a7
Amogh <amoghraikar21@gmail.com>
2024-12-13 13:25:24 +0530

    Delete getCode.js

--- getCode.js
+++
@@ -1,9 +0,0 @@
-// /api/getCode.js
-let codes = [];  // You can also use a database for persistent storage
-
-module.exports = async (req, res) => {
-  if (req.method === 'GET') {
-    return res.json({ codes });
-  }
-  return res.status(405).json({ success: false, message: 'Method not allowed' });
-};
